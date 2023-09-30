d2e90ed92f64eb3b8881fd67ae570996577ed123
kanishkar <kanishkrish123@gmail.com>
2023-09-30 20:18:34 +0100

    Post CRUD API Base

--- src/main/java/com/sinter/cookspire/service/PostService.java
+++ src/main/java/com/sinter/cookspire/service/PostService.java
@@ -1,5 +1,20 @@
 package com.sinter.cookspire.service;
 
+import java.util.List;
+
+import com.sinter.cookspire.dto.PostDTO;
+import com.sinter.cookspire.dto.ResponseDTO;
+
+import jakarta.validation.Valid;
+
 public interface PostService {
+
+    PostDTO persistPost(PostDTO request);
+
+    PostDTO fetchPost(@Valid Long userId);
+
+    List<PostDTO> fetchAllPost(@Valid Long userId);
+
+    ResponseDTO deletePost(@Valid Long userId);
 
 }
