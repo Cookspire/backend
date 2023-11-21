53b6869f89964975d9ff302dc443e031d9a2ff4c
kanishkar <kanishkrish123@gmail.com>
2023-11-21 16:05:55 +0000

    Changes for follow users

--- src/main/java/com/sinter/cookspire/dto/FollowersDataDTO.java
+++ src/main/java/com/sinter/cookspire/dto/FollowersDataDTO.java
@@ -1,5 +1,7 @@
 package com.sinter.cookspire.dto;
 
+import java.time.LocalDateTime;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,9 +10,25 @@ import lombok.Data;
 public class FollowersDataDTO {
 
     private long id;
-    
-    private String name;
 
     private boolean isVerified;
+
+    private String username;
+
+    private String email;
+
+    private String country;
+
+    private String bio;
+
+    private LocalDateTime createdOn;
+
+    private LocalDateTime updatedOn;
+
+    private String imageName;
+
+    private String imageType;
+
+    private byte[] imageData;
 
 }
