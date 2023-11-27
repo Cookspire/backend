fdf8304f618722b281866be1176292fca5696560
kanishkar <kanishkrish123@gmail.com>
2023-11-27 21:05:54 +0000

    Minor changes

--- src/main/java/com/sinter/cookspire/utils/ApplicationBeans.java
+++ src/main/java/com/sinter/cookspire/utils/ApplicationBeans.java
@@ -39,7 +39,7 @@ public class ApplicationBeans {
 
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:5000/");
+                registry.addMapping("/**").allowedOrigins("http://localhost:5000/","http://localhost:8090/");
             }
         };
     }
