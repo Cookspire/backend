f8a8a5611ff078362e00ff394d3edf905179977f
kanishkar <kanishkrish123@gmail.com>
2023-12-07 14:36:50 +0000

    Changes for XSS attack

--- src/main/java/com/sinter/cookspire/security/SecurityConfig.java
+++ src/main/java/com/sinter/cookspire/security/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.header.writers.XXssProtectionHeaderWriter;
 
 @Configuration
 @EnableWebSecurity
@@ -24,18 +25,41 @@ public class SecurityConfig {
 
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
                 if (profile.equalsIgnoreCase("dev")) {
-                        http.csrf((csrf) -> csrf.disable()).authorizeHttpRequests((requests) -> requests
-                                        .requestMatchers("*")
-                                        .permitAll()
-                                        .anyRequest().permitAll());
+                        http.csrf((csrf) -> csrf.disable())
+                                        .headers(headers -> headers.contentSecurityPolicy(
+                                                        cps -> cps.policyDirectives("script-src 'self' ....."))
+                                                        .xssProtection(
+                                                                        xss -> xss.headerValue(
+                                                                                        XXssProtectionHeaderWriter.HeaderValue.ENABLED_MODE_BLOCK)))
+                                        .authorizeHttpRequests((requests) -> requests
+                                                        .requestMatchers("*")
+                                                        .permitAll()
+                                                        .anyRequest().permitAll());
                 } else {
-                        http.csrf((csrf) -> csrf.disable()).authorizeHttpRequests((requests) -> requests
-                                        .requestMatchers("/verify/user/**", "/persist/user", "/fetchAll/trending/post/**","/refresh/token", "/docs/**",
-                                                        "/swagger-ui/**","/actuators/**","/actuators/prometheus/**",
-                                                        "v3/api-docs/**")
-                                        .permitAll()
-                                        .anyRequest().authenticated()).sessionManagement(
+                        http.csrf((csrf) -> csrf.disable()).headers(headers -> headers.contentSecurityPolicy(
+                                        cps -> cps.policyDirectives("script-src 'self' ....."))
+                                        .xssProtection(
+                                                        xss -> xss.headerValue(
+                                                                        XXssProtectionHeaderWriter.HeaderValue.ENABLED_MODE_BLOCK)))
+                                        .authorizeHttpRequests((requests) -> requests
+                                                        .requestMatchers("/verify/user/**", "/persist/user",
+                                                                        "/fetchAll/trending/post/**", "/refresh/token",
+                                                                        "/docs/**",
+                                                                        "/swagger-ui/**", "/actuators/**",
+                                                                        "/actuators/prometheus/**",
+                                                                        "v3/api-docs/**", "/fetch/trending/profile**",
+                                                                        "/fetchAll/trending/post/**",
+                                                                        "/search/cookspire/**",
+                                                                        "/search/recipe/**", "/filter/recipe/**",
+                                                                        "/search/people/**",
+                                                                        "/search/recipe**", "/fetch/recipe/cuisine/**",
+                                                                        "/filter/recipe", "/fetch/complete/recipe/**",
+                                                                        "/search/cookspire/**")
+                                                        .permitAll()
+                                                        .anyRequest().authenticated())
+                                        .sessionManagement(
                                                         (sessionManage) -> sessionManage.sessionCreationPolicy(
                                                                         SessionCreationPolicy.STATELESS))
                                         .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
